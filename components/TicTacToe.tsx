4ae12d93d3670df465cdfd241b8f42d7e82f64eb
Puyan Wei <puyanwei@gmail.com>
2023-03-27 11:05:43 +0100

    add restart button & some code cleanup

--- components/TicTacToe.tsx
+++ components/TicTacToe.tsx
@@ -1,35 +1,9 @@
-import Ably from 'ably/promises'
-import { useEffect, useState } from 'react'
+import Ably from "ably/promises"
+import { useEffect, useState } from "react"
+import { winningCombinations } from "../const/maps"
 
-const ably = new Ably.Realtime.Promise({ authUrl: '/api/createTokenRequest' })
-const channel = ably.channels.get('tic-tac-toe')
-
-const winningCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-]
-
-type StateType = '' | 'x' | 'o'
-export interface Board {
-  state: StateType
-}
-const initialBoard: Board[] = [
-  { state: '' },
-  { state: '' },
-  { state: '' },
-  { state: '' },
-  { state: '' },
-  { state: '' },
-  { state: '' },
-  { state: '' },
-  { state: '' },
-]
+const ably = new Ably.Realtime.Promise({ authUrl: "/api/createTokenRequest" })
+const channel = ably.channels.get("tic-tac-toe")
 
 export default function TicTacToe() {
   const [board, setBoard] = useState<Board[]>(initialBoard)
@@ -37,8 +11,9 @@ export default function TicTacToe() {
   const [gameOver, setGameOver] = useState<boolean>(false)
 
   useEffect(() => {
-    channel.publish('tic-tac-toe', { board, isPlayerOneTurn, gameOver })
-    channel.subscribe('tic-tac-toe', (msg) => {
+    console.log(`useEffect triggered`)
+    channel.publish("tic-tac-toe", { board, isPlayerOneTurn, gameOver })
+    channel.subscribe("tic-tac-toe", (msg) => {
       if (!msg?.data) return null
       const { board, isPlayerOneTurn, gameOver } = msg.data
       setBoard(board)
@@ -50,19 +25,17 @@ export default function TicTacToe() {
 
   async function handleClick(index: number) {
     const newBoard = [...board]
-    if (newBoard[index].state !== '') return null
-    newBoard[index].state = isPlayerOneTurn ? 'x' : 'o'
-    await ably.channels
-      .get('tic-tac-toe')
-      .publish('tic-tac-toe', { board, isPlayerOneTurn, gameOver })
+    if (newBoard[index].state !== "") return null
+    newBoard[index].state = isPlayerOneTurn ? "x" : "o"
+    // await ably.channels
+    //   .get("tic-tac-toe")
+    //   .publish("tic-tac-toe", { board, isPlayerOneTurn, gameOver })
 
     setPlayerOneTurn(!isPlayerOneTurn)
 
     if (!!checkWinner(board)) {
       setGameOver(true)
-      ably.channels
-        .get('tic-tac-toe')
-        .publish('tic-tac-toe', { board, isPlayerOneTurn, gameOver })
+      ably.channels.get("tic-tac-toe").publish("tic-tac-toe", { board, isPlayerOneTurn, gameOver })
       return null
     }
   }
@@ -70,37 +43,41 @@ export default function TicTacToe() {
   function checkWinner(board: Board[]): boolean {
     return winningCombinations.some((combination) => {
       const [a, b, c] = combination
-      if (
-        board[a].state === '' ||
-        board[b].state === '' ||
-        board[c].state === ''
-      )
-        return false
+      if (board[a].state === "" || board[b].state === "" || board[c].state === "") return false
       return (
-        board[a].state &&
-        board[a].state === board[b].state &&
-        board[a].state === board[c].state
+        board[a].state && board[a].state === board[b].state && board[a].state === board[c].state
       )
     })
   }
 
-  const textColor = gameOver ? 'text-red-200' : 'text-red-500'
-  const buttonStyle = `w-full aspect-square border-black border-[1px] text-9xl pb-8 ${textColor}`
+  function handleRestart() {
+    setBoard([
+      { state: "" },
+      { state: "" },
+      { state: "" },
+      { state: "" },
+      { state: "" },
+      { state: "" },
+      { state: "" },
+      { state: "" },
+      { state: "" },
+    ])
+    setPlayerOneTurn(true)
+    setGameOver(false)
+  }
 
-  if (!!gameOver) channel.unsubscribe('tic-tac-toe')
+  const textColor = gameOver ? "text-red-200" : "text-red-500"
+  const tttButtonStyle = `w-full aspect-square border-black border-[1px] text-9xl pb-8 ${textColor}`
+  const buttonStyle = `py-2 px-4 border-2 rounded border-gray-300 text-gray-600 hover:border-gray-400 font-bold`
 
+  if (!!gameOver) channel.unsubscribe("tic-tac-toe")
   return (
-    <div className='p-4'>
-      <h1 className='text-4xl text-center'>Tic Tac Toe</h1>
-      {gameOver && (
-        <h2 className='pt-4 text-2xl text-center'>
-          Game Over - {isPlayerOneTurn ? 'O' : 'X'} is the winner!
-        </h2>
-      )}
-      <div className='grid grid-cols-3 p-20 pt-12 place-content-center'>
+    <div className="p-4">
+      <h1 className="text-4xl text-center">Tic Tac Toe</h1>
+      <div className="grid max-w-2xl grid-cols-3 p-20 pt-12 mx-auto">
         {board.map(({ state }, index) => (
           <button
-            className={buttonStyle}
+            className={tttButtonStyle}
             key={index}
             onClick={() => handleClick(index)}
             disabled={gameOver}
@@ -109,6 +86,33 @@ export default function TicTacToe() {
           </button>
         ))}
       </div>
+      {gameOver && (
+        <div className="text-center">
+          <h2 className="w-full pt-4 text-2xl">
+            Game Over - {isPlayerOneTurn ? "O" : "X"} is the winner!
+          </h2>
+          <br />
+          <button className={buttonStyle} onClick={handleRestart}>
+            Restart
+          </button>
+        </div>
+      )}
     </div>
   )
 }
+
+type StateType = "" | "x" | "o"
+export interface Board {
+  state: StateType
+}
+const initialBoard: Board[] = [
+  { state: "" },
+  { state: "" },
+  { state: "" },
+  { state: "" },
+  { state: "" },
+  { state: "" },
+  { state: "" },
+  { state: "" },
+  { state: "" },
+]
--- pages/api/createTokenRequest.ts
+++ pages/api/createTokenRequest.ts
@@ -1,13 +1,13 @@
-import Ably from 'ably/promises'
-import { NextApiRequest, NextApiResponse } from 'next'
+import Ably from "ably/promises"
+import { NextApiRequest, NextApiResponse } from "next"
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const client = new Ably.Realtime(process.env.ABLY_API_KEY as string)
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const apiKey = process.env.ABLY_API_KEY
+  if (!apiKey) throw new Error("Missing ABLY_API_KEY env var")
+
+  const client = new Ably.Realtime(apiKey)
   const tokenRequestData = await client.auth.createTokenRequest({
-    clientId: 'tic-tac-toe',
+    clientId: "tic-tac-toe",
   })
   res.status(200).json(tokenRequestData)
 }
